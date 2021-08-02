97909cc8f514284e4dab4b7f6f16a74bcd78cd24
Tyson Wellings <tyson.s3510@gmail.com>
2021-08-02 21:42:01 +1000

    Javascript draft done

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
 var slider = document.getElementById("page-views-slider");
-var selectButton = document.getElementById("slider-visible-thumb");
+var sliderThumb = document.getElementById("slider-visible-thumb");
+var sliderThumbWidth = sliderThumb.offsetWidth
 var filledTrack = document.getElementById("slider-track-filled");
-xPos =["0%", "calc(25% - 8px)", "calc(50% - 16px)", "calc(75% - 24px)","calc(100% - 32px)"] 
-/* had to manually write in the positions for the visible thumb that way the visible thumb would be 
-overlappin the input's orginal thumb. If the overlap wasn't exact there would be portions of the thumb
-that would not be clickable which would drastically hinder the user experience */
-slider.oninput = function(){
-    selectButton.style.left = xPos[this.value]
-    filledTrack.style.width = this.value*25 + "%"
+var pageViews = ["10K", "50K", "100K", "500K", "1M"]
+var monthlyRate = [8, 12, 16, 24, 36]
+var discountSwitch = document.getElementById("switch-toggle")
+
+var xPos = []
+for (i = 0; i <= parseInt(slider.max); i++){
+    prepareXposCoordinates(i)
 }
+
+function init (){
+    prepareSlider()
+    prepareDiscountSwitch()
+}
+/* had to write in the positions for the visible thumbs using a for loop 
+that iterated through a function that way the visible thumb would be
+overlapping the input's orginal thumb irrespective of screen-size. If the overlap wasn't exact there 
+would be portions of the thumb that would not be clickable which could 
+hinder the user experience imo */
+function prepareSlider(){
+    slider.oninput = function(){
+    sliderThumb.style.left = xPos[this.value]
+    filledTrack.style.width = this.value*25 + "%"
+    calculateRate()
+    changePageViews()
+    }
+}
+
+
+function prepareXposCoordinates (index){
+    var percentage, offset 
+   /*  percentage refering to percentage width of the slider offset referring to
+     how offset the thumb will be at each point of the slider */
+     percentage = 25*index
+     offset = sliderThumbWidth*index/4
+     xPos[index] = "calc(" + percentage + "% - " + offset + "px)"
+ /*     the xPos is written using the calc() css syntax. */
+}
+
+function calculateRate (){
+    if (discountSwitch.checked == false){
+        document.getElementById("price").innerText = "$" + monthlyRate[slider.value] + ".00"
+        return
+    }
+        document.getElementById("price").innerText = "$" + monthlyRate[slider.value]*9 + ".00"
+    
+}
+
+function changePageViews(){
+    document.getElementById("page-views-text").innerText = pageViews[slider.value] + " PAGEVIEWS"
+}
+
+function prepareDiscountSwitch(){
+    discountSwitch.addEventListener("input",function(){
+        if (discountSwitch.checked == true){
+            document.getElementById("pricing-term").innerText = "year"
+            calculateRate()
+            return
+        }
+
+        document.getElementById("pricing-term").innerText = "month"
+        calculateRate()
+    })
+}
+
+document.addEventListener('DOMContentLoaded', init);
